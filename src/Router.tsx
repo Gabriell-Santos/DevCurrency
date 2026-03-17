2cab6bc704571c96e2e6801294e02b3410536bb5
Gabriell-Santos <ferreirag.santos.dev@gmail.com>
2026-03-17 12:59:26 -0300

    Craido o componente layout e o header

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,8 +2,10 @@ import { createBrowserRouter } from "react-router-dom";
 import { Home } from "./Pages/Home";
 import { Detail } from "./Pages/Detail";
 import { NotFound } from "./Pages/NotFound";
+import { Layout } from "./Components/Layout";
 const Router = createBrowserRouter([
   {
+    element: <Layout />,
     children: [
       {
         path: "/",
