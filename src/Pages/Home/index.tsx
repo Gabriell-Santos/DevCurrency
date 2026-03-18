d6d2665cb6111b0d78731036f1338737268c9e31
Gabriell-Santos <ferreirag.santos.dev@gmail.com>
2026-03-18 12:31:49 -0300

    desenvolvido a estrutura da page Home

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,7 +1,14 @@
+import { BsSearch } from "react-icons/bs";
+import style from "./style.module.css";
 export function Home() {
   return (
-    <div>
-      <h2> Bem - Vindo ao DevCurrency </h2>
-    </div>
+    <main className={style.container}>
+      <form className={style.form}>
+        <input type="text" placeholder="Digite o nome da moeda" />
+        <button type="submit">
+          <BsSearch size={10} color="#fff" />
+        </button>
+      </form>
+    </main>
   );
 }
