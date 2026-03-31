54dc39d8cb51f37f43e2fe834453033687fb09a1
Gabriell-Santos <ferreirag.santos.dev@gmail.com>
2026-03-31 12:56:48 -0300

    feita a tela not found e feito o layout dessa tela para o mobile

--- src/Pages/NotFound/index.tsx
+++ src/Pages/NotFound/index.tsx
@@ -1,8 +1,10 @@
 import style from "./style.module.css";
+import { Link } from "react-router-dom";
 export function NotFound() {
   return (
     <div className={style.container}>
       <h2>404 - Página Não Encontrada</h2>
+      <Link to={"/"}> Voltar Para Página Principal </Link>
     </div>
   );
 }
